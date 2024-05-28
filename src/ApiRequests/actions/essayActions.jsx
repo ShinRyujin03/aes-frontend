82e4ba5a144219062874fe4b7756bcd8cab67083
giabinh <truonggiabjnhcute2003@gmail.com>
2024-05-29 00:03:02 +0700

    check-asdf

--- src/ApiRequests/actions/essayActions.jsx
+++ src/ApiRequests/actions/essayActions.jsx
@@ -1,25 +1,27 @@
 import axios from "axios";
 import {
-	ESSAY_SCORING_FAIL,
-	ESSAY_SCORING_REQUEST,
-	ESSAY_SCORING_SUCCESS,
+    ESSAY_SCORING_FAIL,
+    ESSAY_SCORING_REQUEST,
+    ESSAY_SCORING_SUCCESS,
 } from "../constants/essayConstants";
 
 export const createEssayScore = (essayData) => async (dispatch) => {
-	try {
-		dispatch({ type: ESSAY_SCORING_REQUEST });
-		const { data } = await axios.post("/api/check-grammar/", essayData);
-		dispatch({
-			type: ESSAY_SCORING_SUCCESS,
-			payload: data,
-		});
-	} catch (error) {
-		dispatch({
-			type: ESSAY_SCORING_FAIL,
-			payload:
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		});
-	}
+    try {
+        dispatch({ type: ESSAY_SCORING_REQUEST });
+
+        const { data } = await axios.post('https://aes-backend.vercel.app/check-grammar/', essayData);
+        console.log("Response Data:", data); // Log the response data
+        dispatch({
+            type: ESSAY_SCORING_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: ESSAY_SCORING_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
 };
--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				//don't say localhost
-				target: "https://aes-backend.vercel.app",
-				// target: "http://127.0.0.1:8000",
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
+	// server: {
+	// 	proxy: {
+	// 		"/api": {
+	// 			//don't say localhost
+	// 			target: "https://aes-backend.vercel.app",
+	// 			// target: "http://127.0.0.1:8000",
+	// 			changeOrigin: true,
+	// 			secure: false,
+	// 			rewrite: (path) => path.replace(/^\/api/, ""),
+	// 		},
+	// 	},
+	// },
 });
