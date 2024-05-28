7f708d99fdf54ef795f45ad4a79612fd33bfec8d
giabinh <truonggiabjnhcute2003@gmail.com>
2024-05-29 00:13:32 +0700

    Update with .env

--- src/ApiRequests/actions/essayActions.jsx
+++ src/ApiRequests/actions/essayActions.jsx
@@ -1,27 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
 import {
-    ESSAY_SCORING_FAIL,
-    ESSAY_SCORING_REQUEST,
-    ESSAY_SCORING_SUCCESS,
-} from "../constants/essayConstants";
+  ESSAY_SCORING_FAIL,
+  ESSAY_SCORING_REQUEST,
+  ESSAY_SCORING_SUCCESS,
+} from '../constants/essayConstants';
 
 export const createEssayScore = (essayData) => async (dispatch) => {
-    try {
-        dispatch({ type: ESSAY_SCORING_REQUEST });
+  try {
+    dispatch({ type: ESSAY_SCORING_REQUEST });
 
-        const { data } = await axios.post('https://aes-backend.vercel.app/check-grammar/', essayData);
-        console.log("Response Data:", data); // Log the response data
-        dispatch({
-            type: ESSAY_SCORING_SUCCESS,
-            payload: data,
-        });
-    } catch (error) {
-        dispatch({
-            type: ESSAY_SCORING_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    const baseURL = import.meta.env.VITE_API_BASE_URL;
+    const postURL = `${baseURL}/check-grammar/`;
+
+    console.log("API Post URL:", postURL); // Debug log for the URL
+
+    const { data } = await axios.post(postURL, essayData);
+    console.log("Response Data:", data); // Log the response data
+
+    dispatch({
+      type: ESSAY_SCORING_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    console.error("API Error:", error); // Debug log for the error
+    dispatch({
+      type: ESSAY_SCORING_FAIL,
+      payload: 
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	// server: {
-	// 	proxy: {
-	// 		"/api": {
-	// 			//don't say localhost
-	// 			target: "https://aes-backend.vercel.app",
-	// 			// target: "http://127.0.0.1:8000",
-	// 			changeOrigin: true,
-	// 			secure: false,
-	// 			rewrite: (path) => path.replace(/^\/api/, ""),
-	// 		},
-	// 	},
-	// },
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_API_BASE_URL,
+					changeOrigin: true,
+					secure: false,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
+			},
+		},
+	};
 });
